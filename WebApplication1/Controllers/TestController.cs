f9915671af02f12f43930407870fbd7af5b115f4
Makar Pundzis <makar.pundzis@itechart-group.com>
2022-01-12 15:08:53 +0300

    init

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly string _requestUrl = "https://www.cbr.ru/scripts/XML_daily.asp";
+
+        [HttpGet]
+        public async Task<IActionResult> Index(DateTime? dateTime)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync(_requestUrl + "?date_req=02/03/2002");
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var contentStreamReader = new StreamReader(response.Content.ReadAsStream(), Encoding.GetEncoding("windows-1251"));
+
+            XmlSerializer serializer = new(typeof(List<Rate>), new XmlRootAttribute("ValCurs"));
+            try
+            {
+                using var stream = contentStreamReader;
+                var rates = (List<Rate>)serializer.Deserialize(stream);
+                return Ok(rates);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ WebApplication1/Models/Rate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace WebApplication1.Models
+{
+    [XmlType("Valute")]
+    [Serializable]
+    public class Rate
+    {
+        [XmlAttribute("ID")]
+        public string Id { get; set; }
+        [XmlElement("NumCode")]
+        public string NumCode { get; set; }
+        [XmlElement("CharCode")]
+        public string CharCode { get; set; }
+        [XmlElement("Nominal")]
+        public string Nominal { get; set; }
+        [XmlElement("Name")]
+        public string Name { get; set; }
+        [XmlElement("Value")]
+        public string Value { get; set; }
+    }
+}
